80aaefd72cb80418723f879b567e55d1b27e5310
ikoote1 <ikootepreim@gmail.com>
2023-08-16 01:29:19 +0300

    node

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+// src/redux/store.js
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import reducer from './reducer';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+export default store;
